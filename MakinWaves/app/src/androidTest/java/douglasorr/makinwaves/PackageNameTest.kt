3fdbf5be0031b42c101abda4067699e295713261
Douglas Orr <douglas.orr99@gmail.com>
2020-04-04 12:33:37 +0100

    Initial commit of MakinWaves - a modified fullscreen activity template

---
+++ MakinWaves/app/src/androidTest/java/douglasorr/makinwaves/PackageNameTest.kt
@@ -0,0 +1,18 @@
+package douglasorr.makinwaves
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+@RunWith(AndroidJUnit4::class)
+class PackageNameTest {
+    @Test
+    fun useAppContext() {
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("douglasorr.makinwaves", appContext.packageName)
+    }
+}
---
+++ MakinWaves/app/src/test/java/douglasorr/makinwaves/DummyUnitTest.kt
@@ -0,0 +1,12 @@
+package douglasorr.makinwaves
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class DummyUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
