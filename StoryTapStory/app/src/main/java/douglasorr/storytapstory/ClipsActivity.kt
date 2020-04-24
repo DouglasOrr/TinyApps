ee63d50ce0804ad4a222bd8f211388307e296de6
Douglas Orr <douglas.orr99@gmail.com>
2020-04-24 06:33:55 +0100

    Add stub clip-recording activity

---
+++ StoryTapStory/app/src/main/java/douglasorr/storytapstory/ClipsActivity.kt
@@ -0,0 +1,11 @@
+package douglasorr.storytapstory
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ClipsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_clips)
+    }
+}
